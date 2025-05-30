64bd516309211821d780d91b453fcce2bfbc21e0
Jesus Perez <ios.developer.jesus@gmail.com>
2025-05-30 13:04:14 -0600

    Initial Commit

---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by Jesus Perez on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginViewTests/LoginViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewTests.swift
+//  LoginViewTests
+//
+//  Created by Jesus Perez on 30/05/25.
+//
+
+import Testing
+@testable import LoginView
+
+struct LoginViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
